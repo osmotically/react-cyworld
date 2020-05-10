552e89deaa372eac30d30206339aa74587c96bac
Lee <osmotically@gmail.com>
2020-05-09 17:51:04 -0700

    Updates

---
+++ precache-manifest.07697b5203047263763d4037a78b18fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0024420d165d2dd420f300f90717b908",
+    "url": "/osmotically/react-cyworld/index.html"
+  },
+  {
+    "revision": "f8a5cc20776e443a4065",
+    "url": "/osmotically/react-cyworld/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc3479dcf1abfaa1f739",
+    "url": "/osmotically/react-cyworld/static/js/2.99a1c58c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/osmotically/react-cyworld/static/js/2.99a1c58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a5cc20776e443a4065",
+    "url": "/osmotically/react-cyworld/static/js/main.60af374f.chunk.js"
+  },
+  {
+    "revision": "a13f9539f9c5a1d79cc3",
+    "url": "/osmotically/react-cyworld/static/js/runtime-main.0cf4f3e2.js"
+  }
+]);
