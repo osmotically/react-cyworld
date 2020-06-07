0c1f7e966f4b90a72a370169a5f21a3f6df4019a
Lee <osmotically@gmail.com>
2020-06-07 03:37:24 -0700

    Updates

--- precache-manifest.00981c0cd539184fedaf8a4f3aa62db1.js
+++ precache-manifest.00981c0cd539184fedaf8a4f3aa62db1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aad1279d33b3ab712899fe182f0af8d0",
+    "revision": "3a3ba05eac9df82ebf0f4ce242fa1d43",
     "url": "/react-cyworld/index.html"
   },
   {
-    "revision": "37fe2ab82d39afafcce5",
+    "revision": "6b3484b0fc311a00a3c5",
     "url": "/react-cyworld/static/css/2.35fcc229.chunk.css"
   },
   {
-    "revision": "bbc395acbec1b782990a",
+    "revision": "cbe1f645b947b4c8e19b",
     "url": "/react-cyworld/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "37fe2ab82d39afafcce5",
-    "url": "/react-cyworld/static/js/2.852f762e.chunk.js"
+    "revision": "6b3484b0fc311a00a3c5",
+    "url": "/react-cyworld/static/js/2.5294ed63.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/react-cyworld/static/js/2.852f762e.chunk.js.LICENSE.txt"
+    "url": "/react-cyworld/static/js/2.5294ed63.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bbc395acbec1b782990a",
-    "url": "/react-cyworld/static/js/main.d4f78b48.chunk.js"
+    "revision": "cbe1f645b947b4c8e19b",
+    "url": "/react-cyworld/static/js/main.759c0765.chunk.js"
   },
   {
     "revision": "cf4ac4b671962de4e5a9",
