eeb7ae3688eeb870bb96451025b7997dcc43ee8b
Lee <osmotically@gmail.com>
2020-06-14 15:28:36 -0700

    Adding a divider css + shadow effect

--- src/CyworldMiniHomeLeftPanel.tsx
+++ src/CyworldMiniHomeLeftPanel.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import CyworldMiniHomeLeftPanelPicture from './CyworldMiniHomeLeftPanelPicture';
 import CyworldMiniHomeLeftPanelTodayIs from './CyworldMiniHomeLeftPanelTodayIs';
 import CyworldMiniHomeLeftPanelIntro from './CyworldMiniHomeLeftPanelIntro';
-import { Divider } from 'antd';
+
+import styles from './Cyworld.module.css';
 
 function CyworldMiniHomeLeftPanel() {
   return (
     <div style={{ textAlign: "center" }}>
       <CyworldMiniHomeLeftPanelPicture />
-      <Divider style={{ marginTop: '12px', marginBottom: '4px' }} dashed />
+      <div className={styles.homeLeftPanelDivider} />
       <CyworldMiniHomeLeftPanelTodayIs />
       <CyworldMiniHomeLeftPanelIntro />
     </div>
